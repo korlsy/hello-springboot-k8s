c631df3e64f42feadfad69c96b4a0d76705c9b02
sangyeol.lee <korlsy@gmail.com>
2025-08-10 13:40:41 +0900

    c

---
+++ src/main/java/com/lee/HelloSpringbootK8sApplication.java
@@ -0,0 +1,13 @@
+package com.lee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringbootK8sApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringbootK8sApplication.class, args);
+	}
+
+}
